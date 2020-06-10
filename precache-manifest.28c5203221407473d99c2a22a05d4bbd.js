b3c30b13d52d8525c1c26978f7c87edf0e97106f
Alexei Darmin <alexei.darmin@gmail.com>
2020-06-10 16:55:48 -0400

    Updates

---
+++ precache-manifest.28c5203221407473d99c2a22a05d4bbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83284552c00249510e8bae8f36803e1c",
+    "url": "/contagion/index.html"
+  },
+  {
+    "revision": "2ea237425bb841588b26",
+    "url": "/contagion/static/css/main.f5cc6954.chunk.css"
+  },
+  {
+    "revision": "d4628b27c0bc1b09e5b6",
+    "url": "/contagion/static/js/2.ce04563d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/contagion/static/js/2.ce04563d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea237425bb841588b26",
+    "url": "/contagion/static/js/main.494b9dc3.chunk.js"
+  },
+  {
+    "revision": "38319cb98038129af192",
+    "url": "/contagion/static/js/runtime-main.2a021b25.js"
+  }
+]);
